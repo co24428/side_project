edfd2ed3886663f3dac6aea9df92ca98232e81ff
IanKim <ihwan1236@gmail.com>
2024-08-21 00:25:17 -0700

    ParlindromeChecker - TODO: ASCII Code

--- PalindromeChecker/script.js
+++ PalindromeChecker/script.js
@@ -1,10 +1,14 @@
 function checkPalindrome(){
     const textElem = document.getElementById("text-input");
     const text = preprocessing(textElem.value);
+    if (text === ""){
+        alert("Please input a value");
+        return 0;
+    }
     const lenOfText = text.length;
     const numberOfloop = Math.floor(lenOfText/2);
     let isPalindrome = true;
-    const resultElem = document.getElementsByClassName("result");
+    const resultElem = document.getElementById("result");
 
     for (let i=0; i<numberOfloop; i++){
         if (text[i] === text[lenOfText - 1 - i]){
@@ -14,15 +18,20 @@ function checkPalindrome(){
             break;
         }
     }
-    resultElem[0].children[0].innerText = textElem.value;
     if (isPalindrome) {
-        resultElem[0].children[1].innerText = "is a palindrome";
+        resultElem.innerText = `${text} is a palindrome`;
     } else {
-        resultElem[0].children[1].innerText = "is not a palindrome";
+        resultElem.innerText = `${text} is not a palindrome`;
+
     }
     textElem.value = "";
 }
-
+// TODO: Check punctuation: Check ASCII code...!
+// _eye (A palindrome)
+// A man, a plan, a canal. Panama (A palindrome)
+// My age is 0, 0 si ega ym. (A palindrome)
+// 0_0 (: /-\ :) 0-0 (A palindrome)
+// five|\_/|four (Not a palindrome)
 function preprocessing(str){
     // change to lowercase & remove black characters 
     return str.toLowerCase().split(' ').join('');
